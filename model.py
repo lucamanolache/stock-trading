ee85deb487c91f974e1d050aa8fb7804a2fe5d26
Luca Manolache <luca1230@gmail.com>
2022-02-01 14:18:37 -0800

    Formatted code

--- model.py
+++ model.py
@@ -8,12 +8,22 @@ class LSTM(nn.Module):
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
 
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(
+            input_dim,
+            hidden_dim,
+            num_layers,
+            batch_first=True)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        h0 = torch.zeros(
+            self.num_layers,
+            x.size(0),
+            self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(
+            self.num_layers,
+            x.size(0),
+            self.hidden_dim).requires_grad_()
         out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
         out = self.fc(out[:, -1, :])
         return out
